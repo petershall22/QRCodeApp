1576c42955d259284d7411efcbc826009d6958ec
Mohamed Ouahchia <peterrbxl@gmail.com>
2021-07-09 19:51:33 +0100

    initial files

--- lib/initialsetup.dart
+++ lib/initialsetup.dart
@@ -13,9 +13,9 @@ class Setup extends StatefulWidget {
 
 class _SetupState extends State<Setup> {
   final controller = TextEditingController();
-  final page1Visible = true;
-  final page2Visible = false;
-  final page3Visible = false;
+  var page1Visible = true;
+  var page2Visible = false;
+  var page3Visible = false;
 
   void dipose() {
     controller.dispose();
@@ -35,6 +35,20 @@ class _SetupState extends State<Setup> {
 
   void _updatePosition(int position) {
     setState(() => _currentStage = _validPosition(position));
+    if (_currentStage == 0) {
+      page1Visible = true;
+      page2Visible = false;
+      page3Visible = false;
+    }
+    if (_currentStage == 1) {
+      page1Visible = false;
+      page2Visible = true;
+      page3Visible = false;
+    } else if (_currentStage == 2) {
+      page1Visible = false;
+      page2Visible = false;
+      page3Visible = true;
+    }
   }
 
   String currentPagePresentable() {
@@ -43,6 +57,13 @@ class _SetupState extends State<Setup> {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w500,
+        fontSize: 20,
+      ),
+    );
     return MaterialApp(
       home: Scaffold(
         body: Stack(children: [
@@ -87,7 +108,7 @@ class _SetupState extends State<Setup> {
                   ),
                 ),
                 Positioned.fill(
-                  bottom: 170,
+                  bottom: 70,
                   child: Align(
                     alignment: Alignment.center,
                     child: Container(
@@ -105,6 +126,50 @@ class _SetupState extends State<Setup> {
                           ),
                         ),
                       ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 190,
+            left: 68,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Visibility(
+                  visible: true,
+                  child: Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        style: buttonStyle,
+                        child: Text('Next'),
+                        onPressed: () {
+                          _currentStage = _currentStage.ceil();
+                          _updatePosition(max(--_currentStage, 0));
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(25),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: ElevatedButton(
+                      style: buttonStyle,
+                      child: Text('Next'),
+                      onPressed: () {
+                        _currentStage = _currentStage.ceil();
+                        _updatePosition(min(
+                          ++_currentStage,
+                          _stages.toInt(),
+                        ));
+                      },
                     ),
                   ),
                 ),
